10d3acc32406780f82ffc305adf14aa3d252b27e
Nguyễn Như Thịnh <nhuthinh0258@gmail.com>
2022-10-29 03:20:24 +0700

    up

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '192.168.1.14';
-const port = 3000;
+const port = 3030;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
